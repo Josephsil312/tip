7b15220fb4a07003fbadec7e00de32cc67611d64
abhinav19112021 <abhinav.parashar.dorbit@doodle.com>
2022-09-25 23:51:54 +0530

    final design and functionality for production

--- components/Right/Right.jsx
+++ components/Right/Right.jsx
@@ -1,5 +1,4 @@
-
-import { Pressable, StyleSheet, Text, View, Share, Button } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 const Right = (props) => {
 
     let btntipp = props.btntip;
@@ -13,109 +12,32 @@ const Right = (props) => {
         totaltip = btntipp.toFixed(2)
     }
 
-
     return (
         <>
-            <View style={styles.maincontent}>
-                <View style={styles.tip}>
-                    <Text style={styles.tipamt}>Tip Amount</Text>
-                    <Text style={styles.tipamt1}>/ person</Text>
-                    <Text style={styles.billl1}>${(totaltip / Number(numofpeep)).toFixed(2)}</Text>
+            <View style={{ backgroundColor: '#0a94a3', borderRadius: 10, flex: 2, marginTop: 20, padding: 10 }}>
+                <View style={{ justifyContent: 'space-between', flexDirection: 'row'}}>
+                    <View style={{ flexDirection: 'column' }}>
+                        <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 18 }}>Tip Amount</Text>
+                        <Text style={{ color: '#8cedf6', fontWeight: 'bold', fontSize: 15,marginBottom:15 }}>/ person</Text>
+                    </View>
+                    <Text style={{ color: '#8cedf6', fontWeight: 'bold', fontSize: 20 }}>${(totaltip / Number(numofpeep)).toFixed(2)}</Text>
                 </View>
-                <Text style={styles.totall}>Total</Text>
-                <Text style={styles.totall1}>/ person</Text>
-                <View><Text style={styles.final}>${(((Number(totaltip) + Number(bill)) / Number(numofpeep)).toFixed(2))}</Text></View>
-                <Pressable style={styles.reset} android_ripple={{ color: '#065f46' }} onPress={props.handleReset}><Text style={styles.reset_text}>Reset</Text></Pressable>
+                <View style={{ justifyContent: 'space-between', flexDirection: 'row' }}>
+                    <View style={{ flexDirection: 'column' }}>
+                        <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 18 }}>Total</Text>
+                        <Text style={{ color: '#8cedf6', fontWeight: 'bold', fontSize: 15 }}>/ person</Text>
+                    </View>
+                    <Text style={{ color: '#8cedf6', fontWeight: 'bold', fontSize: 20 }}>${(((Number(totaltip) + Number(bill)) / Number(numofpeep)).toFixed(2))}</Text>
+                </View>
+                <TouchableOpacity onPress={props.handleReset}>
+                    <View>
+                        <Text style={{ marginTop:25,borderRadius: 10, paddingVertical: 10, paddingHorizontal: 10, backgroundColor: '#8cedf6', color: '#0a94a3', width: 280, height: 50, textAlign: 'center', fontSize: 22, fontWeight: 'bold' }}>Reset</Text>
+                    </View>
+                </TouchableOpacity>
             </View>
-
         </>
     )
 }
 
 export default Right
 
-const styles = StyleSheet.create({
-    maincontent: {
-        shadowColor: '#52006A',
-        elevation: 20,
-        width: '93%',
-        height: 195,
-        position: 'absolute',
-        top:'65%',
-        backgroundColor: '#059669',
-        borderRadius: 30,
-        marginTop: 28,
-        paddingHorizontal: 10,
-        paddingVertical: 5,
-    },
-    totall1: {
-        position: 'relative',
-        fontSize: 15,
-        color: '#006400',
-        top: 3,
-        left: 10
-    },
-
-    tipamt1: {
-        position: 'relative',
-        fontSize: 15,
-        color: '#006400',
-        top: 11,
-        left: 10
-    },
-    reset_text: {
-        color: 'white',
-        fontSize: 20,
-        fontStyle: 'bold',
-        textAlign: 'center',
-        paddingVertical: 10
-    },
-    reset: {
-        width: '95%',
-        height: 50,
-        position: 'absolute',
-        backgroundColor: '#064e3b',
-        fontWeight: 'bold',
-        borderRadius: 20,
-        color: 'white',
-        position: 'absolute',
-        left: 17,
-        top: 134,
-    },
-    tipamt: {
-        position: 'relative',
-        top: 8,
-        left: 10,
-        color: 'white',
-        fontSize: 17,
-        fontWeight: 'bold'
-    },
-
-    final: {
-        fontSize: 22,
-        position: 'relative',
-        color: '#065f46',
-        fontWeight: 'bold',
-        left: 158,
-        top: -28,
-        height: 500,
-        width: 300,
-        right: 1
-    },
-    billl1: {
-        fontSize: 22,
-        position: 'relative',
-        color: '#065f46',
-        left: 160,
-        top: -20,
-        fontWeight: 'bold'
-    },
-    totall: {
-        fontSize: 17,
-        position: 'relative',
-        color: 'white',
-        left: 10,
-        top: 2,
-        fontWeight: 'bold'
-    },
-})
--- components/left/Left.jsx
+++ components/left/Left.jsx
@@ -3,12 +3,11 @@
 
 import { useState } from "react"
 
-import { View, Text, Pressable, StyleSheet, TextInput, Alert,Button } from 'react-native'
+import { Alert, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import Right from "../Right/Right"
 const Left = () => {
 
   const [bill, setBill] = useState('')
-
   const [btntip, setBtntip] = useState(0)
   const [count, setCount] = useState(1)
   const [tipPercent, setTipPercent] = useState(0)
@@ -47,239 +46,44 @@ const Left = () => {
       setCount(prevCount => prevCount - 1)
     }
   }
-
+  
   return (
     <>
-
-      <View style={styles.container}>
-
-        <View style={styles.maincontent}>
-          <Text style={styles.bill}>Tip Calculator</Text>
-          <TextInput keyboardType='numeric' maxLength={6} placeholder="Enter your bill" style={styles.billentry} value={bill} onChangeText={newText => onChangeHandle(newText)} />
-          <Text style={styles.percenttip}>
-            Select tip %
-          </Text>
-          <View style={styles.boxes1}>
-            <Button style={styles.bt1}  onPress={() => handleTip(0.05)} title = '5%' />
-            <Pressable style={styles.bt2} android_ripple={{ color: '#6ee7b7' }} onPress={() => handleTip(0.10)} ><Text style={styles.btntext}>10%</Text></Pressable>
-            <Pressable style={styles.bt5} android_ripple={{ color: '#6ee7b7' }} onPress={() => handleTip(0.15)} ><Text style={styles.btntext}>15%</Text></Pressable>
-            <TextInput keyboardType='numeric' maxLength={2} placeholder="Custom" style={styles.billentrypercent} value={tipPercent} onChangeText={newTextt => handleChange(newTextt)} />
-          </View>
-
-          <Text style={styles.split}>Split/person</Text>
-          <View style={styles.boxes2}>
-            <Pressable onPress={handleIncrement} android_ripple={{ color: '#6ee7b7' }} style={styles.count_pos}><Text style={styles.plus}>+</Text></Pressable>
-            <View><Text style={styles.counter}>{count}</Text></View>
-            <Pressable onPress={handleDecrement} android_ripple={{ color: '#6ee7b7' }} style={styles.count_neg} ><Text style={styles.minus}>-</Text></Pressable>
-          </View>
+      <View style={{flex: 1,justifyContent: 'flex-start',padding: 30,marginTop: 20}}>
+        <Text style={{ fontSize: 30, fontWeight: 'bold', color: '#0a94a3', alignSelf: 'center' }}>Tip Calculator</Text>
+        <TextInput keyboardType='numeric' maxLength={12} placeholder="Enter your bill" style={{ width: 300, height: 62, color: '#0a94a3', borderRadius: 10, padding: 5, margin: 20, backgroundColor: 'rgb(187, 200, 209)', fontSize: 22, alignSelf: 'center' }} value={bill} onChangeText={newText => onChangeHandle(newText)} />
+        <Text style={{ alignSelf: 'flex-start', fontSize: 20, color: '#0a94a3', fontWeight: 'bold' }}>Select tip %</Text>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginVertical: 15 }}>
+          <TouchableOpacity onPress={() => handleTip(0.05)}>
+            <Text style={{ paddingVertical: 12, paddingHorizontal: 10, backgroundColor: '#0a94a3', color: 'white', width: 130, height: 50, textAlign: 'center', borderRadius: 10, fontSize: 18, fontWeight: 'bold' }} >5%</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => handleTip(0.10)}>
+            <Text style={{ paddingVertical: 12, paddingHorizontal: 10, backgroundColor: '#0a94a3', color: 'white', width: 130, height: 50, textAlign: 'center', borderRadius: 10, fontSize: 18, fontWeight: 'bold' }} >10%</Text></TouchableOpacity>
+        </View>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <TouchableOpacity onPress={() => handleTip(0.15)}>
+            <View>
+              <Text style={{ borderRadius: 10, paddingVertical: 12, paddingHorizontal: 10, backgroundColor: '#0a94a3', color: 'white', width: 130, height: 50, textAlign: 'center', fontSize: 18, fontWeight: 'bold' }}>15%</Text></View>
+          </TouchableOpacity>
+          <TextInput keyboardType='numeric' maxLength={2} placeholder="Custom" style={{ width: 130, height: 50, color: '#0a94a3', borderRadius: 10, backgroundColor: 'rgb(187, 200, 209)', fontSize: 20, padding: 5 }} value={tipPercent} onChangeText={newTextt => handleChange(newTextt)} />
+        </View>
+        <Text style={{ alignSelf: 'flex-start', fontSize: 20, color: '#0a94a3', fontWeight: 'bold', marginVertical: 15 }}>Split/person</Text>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <TouchableOpacity onPress={handleIncrement}>
+            <Text style={{ paddingVertical: 12, paddingHorizontal: 10, backgroundColor: '#0a94a3', color: 'white', width: 110, height: 50, textAlign: 'center', borderRadius: 10, fontSize: 20, fontWeight: 'bold' }} >+</Text>
+          </TouchableOpacity>
+          <Text style={{ color: '#0a94a3', fontSize: 22, justifyContent: 'center', alignSelf: 'center' }}>{count}</Text>
+          <TouchableOpacity onPress={handleDecrement}>
+            <View><Text style={{ borderRadius: 10, paddingVertical: 12, paddingHorizontal: 10, backgroundColor: '#0a94a3', color: 'white', width: 110, height: 50, textAlign: 'center', fontSize: 20, fontWeight: 'bold' }}  >-</Text></View>
+          </TouchableOpacity>
         </View>
         {
           <Right btntip={btntip} handleReset={handleReset} numOfPeople={count} bill={bill} tipPercent={tipPercent} />
         }
-
       </View>
     </>
   )
 }
 
 export default Left
-
-const styles = StyleSheet.create({
-  container: {
-    marginVertical: '20%',
-    padding: 10,
-   
-    flex:1,
-    borderRadius: 40,
-    backgroundColor:'cyan',
-    alignContent:'center',
-    alignSelf:'center'
-},
-  
-  boxes2: {
-    position: 'relative',
-    alignContent: 'center',
-    elevation: 20,
-  },
-  plus: {
-    color: 'white',
-    height: 30,
-    width: 25,
-
-    fontSize: 23,
-    position: 'relative',
-    left:35,
- 
-    
-
-  },
-  minus: {
-    color: 'white',
-    fontSize: 33,
-    width: 27,
-    position: 'relative',
-    height: 30,
-    width: 30,
-    left: 35,
-    top:-10
-  },
-  split: {
-    position: 'relative',
-    top: -278,
-    left: 0,
-    fontSize: 20,
-    color: '#065f46',
-    fontWeight: 'bold',
-    marginTop: 70
-  },
-  counter: {
-    position: 'relative',
-    left: 143,
-    top: -305,
-    color: '#065f46',
-    fontSize: 20,
-
-  },
-
-  count_pos: {
-    width: 130,
-    height: 50,
-    backgroundColor: '#059669',
-    borderRadius: 20,
-    position: 'relative',
-    top: -265,
-    left: 0,
-    elevation: 20,
-    paddingHorizontal: 20,
-    paddingVertical: 10
-  },
-  count_neg: {
-    width: 130,
-    height: 50,
-    backgroundColor: '#059669',
-    borderRadius: 20,
-    paddingHorizontal: 24,
-    paddingVertical: 10,
-    position: 'relative',
-    top: -343,
-    elevation: 20,
-    left: 165
-  },
-  btntext1: {
-    paddingHorizontal: 19,
-    paddingVertical: 16,
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-    fontSize: 15
-  },
-  btntext: {
-    paddingHorizontal: 10,
-    paddingVertical: 10,
-    color: 'white',
-    textAlign: 'center',
-    fontSize: 15,
-    fontWeight: 'bold',
-  },
-  bill: {
-    position: 'relative',
-    top: -8,
-    fontSize: 30,
-    color: '#065f46',
-    fontWeight: 'bold',
-    marginBottom: 10,
-    marginTop: 10,
-    textAlign: 'center',
-    outline: 'None'
-  },
-  billentry: {
-    width: 300,
-    height: '8%',
-    color: '#065f46',
-    borderRadius: 10,
-    fontSize: 20,
-    backgroundColor: 'rgb(187, 200, 209)',
-    padding: 5,
-    outline: 'none',
-
-
-  },
-  billentrypercent: {
-    width: '47%',
-    height: '13%',
-    color: '#065f46',
-    borderRadius: 20,
-    fontSize: 20,
-    backgroundColor: 'rgb(187, 200, 209)',
-    padding: 10,
-    outline: 'none',
-    position: 'relative',
-    left: '53%',
-    top: '-15%',
-    paddingLeft: 60
-  },
-  percenttip: {
-    position: 'relative',
-    left: 0,
-    top: 20,
-    fontSize: 20,
-    color: '#065f46',
-    fontWeight: 'bold'
-  },
-
-  bt1: {
-    width: '10%',
-    elevation: 20,
-    height: '14%',
-    backgroundColor: '#059669',
-    fontWeight: 'bold',
-    borderRadius: 20,
-    color: 'white',
-    position: 'relative',
-    left: 0,
-    top: '8%',
-  },
-  bt2: {
-    width: '47%',
-    height: '14%',
-    backgroundColor: '#059669',
-    borderRadius: 20,
-    color: 'white',
-    position: 'relative',
-    top: '-6%',
-    left: '53%',
-    outline: 'none',
-    paddingHorizontal: 5,
-    paddingVertical: 5,
-    elevation: 20,
-  },
-
-  bt5: {
-    width: '47%',
-    height: '13%',
-    backgroundColor: '#059669',
-    borderRadius: 20,
-    color: 'white',
-    position: 'relative',
-    left: 0,
-    top: '-2%',
-    outline: 'none',
-    paddingHorizontal: 5,
-    paddingVertical: 5
-  },
-
-
-
-  people: {
-    width: 245,
-    height: 40,
-    borderRadius: 10,
-    padding: 5,
-    position: 'relative',
-    bottom: -90,
-    left: 5,
-  },
-
-});
-
 
