8f625151047c212020e7a405175b44ceb165497c
abhinav19112021 <abhinav.parashar.dorbit@doodle.com>
2022-09-26 09:36:57 +0530

    final design with changes in layout

--- components/Right/Right.jsx
+++ components/Right/Right.jsx
@@ -14,7 +14,7 @@ const Right = (props) => {
 
     return (
         <>
-            <View style={{ backgroundColor: '#0a94a3', borderRadius: 10, flex: 2, marginTop: 20, padding: 10 }}>
+            <View style={{ backgroundColor: '#0a94a3', borderRadius: 10, marginTop: 20, padding: 10 }}>
                 <View style={{ justifyContent: 'space-between', flexDirection: 'row'}}>
                     <View style={{ flexDirection: 'column' }}>
                         <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 18 }}>Tip Amount</Text>
@@ -31,7 +31,7 @@ const Right = (props) => {
                 </View>
                 <TouchableOpacity onPress={props.handleReset}>
                     <View>
-                        <Text style={{ marginTop:25,borderRadius: 10, paddingVertical: 10, paddingHorizontal: 10, backgroundColor: '#8cedf6', color: '#0a94a3', width: 280, height: 50, textAlign: 'center', fontSize: 22, fontWeight: 'bold' }}>Reset</Text>
+                        <Text style={{alignSelf:'center', marginTop:25,borderRadius: 10, paddingVertical: 10, paddingHorizontal: 10, backgroundColor: '#8cedf6', color: '#0a94a3', width: '100%', height: 50, textAlign: 'center', fontSize: 22, fontWeight: 'bold' }}>Reset</Text>
                     </View>
                 </TouchableOpacity>
             </View>
--- components/left/Left.jsx
+++ components/left/Left.jsx
@@ -46,29 +46,29 @@ const Left = () => {
       setCount(prevCount => prevCount - 1)
     }
   }
-  
+
   return (
     <>
       <View style={{flex: 1,justifyContent: 'flex-start',padding: 30,marginTop: 20}}>
         <Text style={{ fontSize: 30, fontWeight: 'bold', color: '#0a94a3', alignSelf: 'center' }}>Tip Calculator</Text>
-        <TextInput keyboardType='numeric' maxLength={12} placeholder="Enter your bill" style={{ width: 300, height: 62, color: '#0a94a3', borderRadius: 10, padding: 5, margin: 20, backgroundColor: 'rgb(187, 200, 209)', fontSize: 22, alignSelf: 'center' }} value={bill} onChangeText={newText => onChangeHandle(newText)} />
-        <Text style={{ alignSelf: 'flex-start', fontSize: 20, color: '#0a94a3', fontWeight: 'bold' }}>Select tip %</Text>
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginVertical: 15 }}>
+        <TextInput keyboardType='numeric' maxLength={12} placeholder="Enter your bill" style={{ width: '100%', height: 62, color: '#0a94a3', borderRadius: 10, padding: 5, margin: 20, backgroundColor: '#8cedf6', fontSize: 22, alignSelf: 'center' }} value={bill} onChangeText={newText => onChangeHandle(newText)} />
+        <Text style={{ alignSelf: 'flex-start', fontSize: 22, color: '#0a94a3', fontWeight: 'bold' }}>Select tip %</Text>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-evenly',marginVertical:15 }}>
           <TouchableOpacity onPress={() => handleTip(0.05)}>
             <Text style={{ paddingVertical: 12, paddingHorizontal: 10, backgroundColor: '#0a94a3', color: 'white', width: 130, height: 50, textAlign: 'center', borderRadius: 10, fontSize: 18, fontWeight: 'bold' }} >5%</Text>
           </TouchableOpacity>
           <TouchableOpacity onPress={() => handleTip(0.10)}>
             <Text style={{ paddingVertical: 12, paddingHorizontal: 10, backgroundColor: '#0a94a3', color: 'white', width: 130, height: 50, textAlign: 'center', borderRadius: 10, fontSize: 18, fontWeight: 'bold' }} >10%</Text></TouchableOpacity>
         </View>
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
           <TouchableOpacity onPress={() => handleTip(0.15)}>
             <View>
               <Text style={{ borderRadius: 10, paddingVertical: 12, paddingHorizontal: 10, backgroundColor: '#0a94a3', color: 'white', width: 130, height: 50, textAlign: 'center', fontSize: 18, fontWeight: 'bold' }}>15%</Text></View>
           </TouchableOpacity>
-          <TextInput keyboardType='numeric' maxLength={2} placeholder="Custom" style={{ width: 130, height: 50, color: '#0a94a3', borderRadius: 10, backgroundColor: 'rgb(187, 200, 209)', fontSize: 20, padding: 5 }} value={tipPercent} onChangeText={newTextt => handleChange(newTextt)} />
+          <TextInput keyboardType='numeric' maxLength={2} placeholder="Custom" style={{ width: 130, height: 50, color: '#0a94a3', borderRadius: 10, backgroundColor: '#8cedf6', fontSize: 20, padding: 5 }} value={tipPercent} onChangeText={newTextt => handleChange(newTextt)} />
         </View>
-        <Text style={{ alignSelf: 'flex-start', fontSize: 20, color: '#0a94a3', fontWeight: 'bold', marginVertical: 15 }}>Split/person</Text>
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <Text style={{ alignSelf: 'flex-start', fontSize: 22, color: '#0a94a3', fontWeight: 'bold', marginVertical: 15 }}>Split/person</Text>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
           <TouchableOpacity onPress={handleIncrement}>
             <Text style={{ paddingVertical: 12, paddingHorizontal: 10, backgroundColor: '#0a94a3', color: 'white', width: 110, height: 50, textAlign: 'center', borderRadius: 10, fontSize: 20, fontWeight: 'bold' }} >+</Text>
           </TouchableOpacity>
